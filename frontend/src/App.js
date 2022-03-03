6a9ab085a5be051b527f1da97c2e07318a457580
Peter-Trang <90750229+Peter-Trang@users.noreply.github.com>
2022-03-03 11:54:53 +0100

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import ShoppingList from './components/ShoppingList/ShoppingList';
-import ShoppingOption from './components/SearchResults/ShoppingOption';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+        <NavBar/>
       <ShoppingList>
       </ShoppingList>
     </div>
