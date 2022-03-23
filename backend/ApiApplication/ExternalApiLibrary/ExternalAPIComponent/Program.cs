06a3ed4726e98bdacc03e569da9b64fbb99a2ec4
LukasMandrup <AU617683@post.au.dk>
2022-03-23 20:55:24 +0100

    Change external API application to a class library.

---
+++ backend/ApiApplication/ExternalApiLibrary/ExternalAPIComponent/Program.cs
@@ -0,0 +1,34 @@
+using System.Web;
+using ExternalAPIComponent.Callers.Salling;
+using Serilog;
+
+namespace ExternalAPIComponent;
+
+internal static class Program
+{
+    public static async Task Main()
+    {
+        // Configure logger for start up
+        BackendLogger.BuildLogger();
+
+        try
+        {
+            SallingProductCaller caller = new();
+
+            var result = await caller.Call(new SallingRequestBuilder().Build());
+
+            result.ForEach(obj => Console.WriteLine(obj.ToString()));
+
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Log.Fatal(e, "Application failed to start");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
