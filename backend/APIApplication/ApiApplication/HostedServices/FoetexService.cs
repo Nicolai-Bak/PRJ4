3e5731f4f8bc68bb6a5b14a4bc54631222b69b66
Nicolai-Bak <nicolai.bak@gmail.com>
2022-04-03 17:58:59 +0200

    Test, products inserted to db

--- backend/APIApplication/ApiApplication/HostedServices/FoetexService.cs
+++ backend/APIApplication/ApiApplication/HostedServices/FoetexService.cs
@@ -11,7 +11,7 @@ using Serilog;
 
 namespace ApiApplication.HostedServices
 {
-    public class FoetexService: IHostedService
+    public class FoetexService : IHostedService
     {
         private readonly IPrisninjaDB _db;
 
@@ -33,98 +33,90 @@ namespace ApiApplication.HostedServices
 
         public async Task DoTask()
         {
-            Store s = new Store()
-            {
-                ID = 9,
-                Brand = "f",
-                Location_X = 1,
-                Location_Y = 2,
-                Address = "virk nu",
-            };
-
-            await _db.InsertStore(s);
-
-            s.ID = 8;
-
-            await _db.InsertStore(s);
-
-
-
             // Configure logger for start up
             BackendLogger.BuildLogger();
 
-            try
+            //try
+            //{
+            //    ///// Products - Salling
+            //}
+            //catch (Exception e)
+            //{
+            //    Log.Fatal(e, "Application failed to start");
+            //}
+            //finally
+            //{
+            //    Log.CloseAndFlush();
+            //}
+
+            SallingProductCaller productCaller = new();
+            SallingRequestBuilder builder = new SallingRequestBuilder();
+            builder.AddInfos()
+                    .AddUnits()
+                    .AddUnitsOfMeasure()
+                    .AddStoreData();
+
+            var products = await productCaller.Call(builder.Build());
+
+            IFilter productFilter = new SallingProductFilter();
+            IConverter productConverter = new SallingProductConverter();
+            var filteredProducts = productFilter.Filter(products);
+            var convertedProducts = productConverter.Convert(filteredProducts);
+
+            ///// Stores - Salling
+            ICaller storeCaller = new SallingStoreCaller();
+            IFilter storeFilter = new SallingStoreFilter();
+            IConverter storeConverter = new SallingStoreConverter();
+
+            var stores = await storeCaller.Call(null);
+            var filteredStores = storeFilter.Filter(stores);
+            var convertedStores = storeConverter.Convert(filteredStores);
+
+            ////// Insert stores
+            //foreach (var s in convertedStores )
+            //{
+            //    ConvertedSallingStore convertedStore = (ConvertedSallingStore)s;
+            //    Store store = new Store()
+            //    {
+            //        ID = convertedStore.ID,
+            //        Brand = convertedStore.Brand,
+            //        Location_X = convertedStore.Location_X,
+            //        Location_Y = convertedStore.Location_Y,
+            //        Address = convertedStore.Address
+            //    };
+            //    await _db.InsertStore(store);
+            //}
+
+            ///// Insert products
+            foreach (var p in convertedProducts)
             {
-                ///// Products - Salling
-                SallingProductCaller productCaller = new();
-                SallingRequestBuilder builder = new SallingRequestBuilder();
-                builder.AddInfos()
-                        .AddUnits()
-                        .AddUnitsOfMeasure()
-                        .AddStoreData();
-                IFilter productFilter = new SallingProductFilter();
-                IConverter productConverter = new SallingProductConverter();
-
-                var products = await productCaller.Call(builder.Build());
-                var filteredProducts = productFilter.Filter(products);
-                var convertedProducts = productConverter.Convert(filteredProducts);
-
-                ///// Stores - Salling
-                ICaller storeCaller = new SallingStoreCaller();
-                IFilter storeFilter = new SallingStoreFilter();
-                IConverter storeConverter = new SallingStoreConverter();
-
-                var stores = await storeCaller.Call(null);
-                var filteredStores = storeFilter.Filter(stores);
-                var convertedStores = storeConverter.Convert(filteredStores);
-
-                ////// Insert stores
-                convertedStores.ForEach(async s =>
+                ConvertedSallingProduct sallingProduct = (ConvertedSallingProduct)p;
+                var product = new Product()
                 {
-                    ConvertedStore convertedStore = (ConvertedStore)s;
-                    Store store = new Store()
-                    {
-                        ID = convertedStore.ID,
-                        Brand = convertedStore.Brand,
-                        Location_X = convertedStore.Location_X,
-                        Location_Y = convertedStore.Location_Y,
-                        Address = convertedStore.Address
-                    };
-                    await _db.InsertStore(store);
-                });
-
-                ///// Insert products
-                convertedProducts.ForEach(convertedProduct =>
+                    EAN = sallingProduct.EAN,
+                    Name = sallingProduct.Name,
+                    Brand = sallingProduct.Brand,
+                    Unit = sallingProduct.Unit,
+                    Measurement = sallingProduct.Measurement
+                };
+                foreach (var s in convertedStores)
                 {
-                    ConvertedSallingProduct sallingProduct = (ConvertedSallingProduct)convertedProduct;
-                    var product = new Product()
+                    ConvertedSallingStore sallingStore = (ConvertedSallingStore)s;
+                    var store = new Store()
                     {
-                        EAN = sallingProduct.EAN,
-                        Name = sallingProduct.Name,
-                        Brand = sallingProduct.Brand,
-                        Unit = sallingProduct.Unit,
-                        Measurement = sallingProduct.Measurement
+                        ID = sallingStore.ID,
+                        Brand = sallingStore.Brand,
+                        Location_X = sallingStore.Location_X,
+                        Location_Y = sallingStore.Location_Y,
+                        Address = sallingStore.Address,
                     };
-                    convertedStores.ForEach(async convertedStore =>
-                    {
-                        Store store = (Store)convertedStore;
-                        if (store.Brand == "foetex")
-                        {
-                            await _db.InsertProduct(product, store.ID, price: sallingProduct.Stores[0].Price);
-                        }
-                    });
-                });
-            }
-            catch (Exception e)
-            {
-                Log.Fatal(e, "Application failed to start");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-            
 
+                    if (store.Brand == "foetex")
+                    {
+                        await _db.InsertProduct(product, store.ID, sallingProduct.Stores.First().Value.Price);
+                    }
+                }
+            }
         }
     }
 }
