32cfe0e02eb802c430c50551b05f0afe7af2907f
Nikolaj Lærke <nikolaj.laerke@gmail.com>
2022-04-04 22:22:05 +0200

    Get options returnerer nu 3 lister. Modeller opdateret

--- backend/ApiApplication/ApiApplication/Controllers/RequestController.cs
+++ backend/ApiApplication/ApiApplication/Controllers/RequestController.cs
@@ -29,86 +29,45 @@ namespace ApiApplication.Controllers
         public async Task<ShoppingOptions> GetOptions(ShoppingList shoppingList)
         {
             CheapestSearcher search = new CheapestSearcher();
-            StoreSearch result = search.FindStore(shoppingList);
+            List<StoreSearch> result = search.FindStores(shoppingList);
+
+            List<StoreSearch> cheapestOptions = new List<StoreSearch>();
+
+            foreach (var storeSearch in result)
+            {
+                cheapestOptions.Add(storeSearch);
+            }
 
             var options = new ShoppingOptions()
             {
-                Cheapest = new ShoppingOption()
-                {
-                    StoreName = result.StoreID.ToString(),
-                    TotalPrice = result.GetTotalPrice(),
-                    TotalDistance = result.Distance,
-                    Products = new List<ProductDTO>()
-                }
+                Cheapest = cheapestOptions,
+                Best = null,
+                Nearest = null
             };
-
-            foreach (var p in result.Products)
-            {
-                options.Cheapest.Products.Add(new ProductDTO()
-                {
-                    Name = p.Name,
-                    Name2 = p.Brand,
-                    Price = p.Price
-                });
-            }
 
             return options;
             
-            // ProductDTO product = new ProductDTO()
-            // {
-            //     Name = "GRAASTEN ITALIENSKSALAT",
-            //     Name2 = "150 g",
-            //     Price = 12.95
-            // };
-            //
-            // ProductDTO product2 = new ProductDTO()
-            // {
-            //     Name = "GRAASTEN ITALIENSKSALAT",
-            //     Name2 = "150 g",
-            //     Price = 12.95
-            // };
-            //
-            // var options = new ShoppingOptions()
-            // {
-            //     Best = new ShoppingOption()
-            //     {
-            //         StoreName = "Rema",
-            //         TotalPrice = "123",
-            //         TotalDistance = "2km",
-            //         Products = new List<ProductDTO>()
-            //         {
-            //             product,
-            //             product2
-            //         }
-            //     }
-            // };
-            // options.Cheapest = options.Nearest = options.Best;
-            //
-            // return options;
         }
     }
+    
 
-    public class ProductDTO
-    {
-        public string Name { get; set; }
-        public string Name2 { get; set; }
-        public double Price { get; set; }
-    }
-
-    public class ShoppingOption
-    {
-        public string StoreName { get; set; }
-        public float TotalPrice { get; set; }
-        public float TotalDistance { get; set; }
-        public List<ProductDTO> Products { get; set; }
-    }
+    //public class ShoppingOption
+    //{
+    //    public string StoreName { get; set; }
+    //    public double TotalPrice { get; set; }
+    //    public double TotalDistance { get; set; }
+    //    public List<ProductSearch> Products { get; set; }
+    //}
 
     public class ShoppingOptions
     {
-        public ShoppingOption Best { get; set; }
-        public ShoppingOption Cheapest { get; set; }
-        public ShoppingOption Nearest { get; set; }
+        public List<StoreSearch> Best { get; set; }
+        public List<StoreSearch> Cheapest { get; set; }
+        public List<StoreSearch> Nearest { get; set; }
     }
+
+    
+
 
     public class ShoppingList
     {
