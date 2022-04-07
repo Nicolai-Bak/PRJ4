e3e1042ec97b30b0d5a67b19b804fbe09403a9b1
Nikolaj Lærke <nikolaj.laerke@gmail.com>
2022-04-07 21:48:38 +0200

    Added GetDataFromStores database call

--- backend/APIApplication/ApiApplication/SearchAlgorithm/Models/StoreSearch.cs
+++ backend/APIApplication/ApiApplication/SearchAlgorithm/Models/StoreSearch.cs
@@ -5,6 +5,8 @@
         public int StoreID { get; set; }
         public double TotalPrice { get; set; }
         public double Distance { get; set; }
+        public string Brand { get; set; }
+        public string Address { get; set; }
         public List<ProductSearch> Products { get; set; }
 
         public StoreSearch(int id)
--- backend/ApiApplication/ApiApplication/Controllers/RequestController.cs
+++ backend/ApiApplication/ApiApplication/Controllers/RequestController.cs
@@ -62,7 +62,7 @@ namespace ApiApplication.Controllers
     public class ShoppingOptions
     {
         public List<StoreSearch> Best { get; set; }
-        public List<StoreSearch> Cheapest { get; set; }
+        public List<StoreSearch> Cheapest { get; set; } 
         public List<StoreSearch> Nearest { get; set; }
     }
 
