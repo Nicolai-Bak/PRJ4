e44821f3938cf7654bb9a783088b63ba4f9ad3a0
Nicolai-Bak <nicolai.bak@gmail.com>
2022-04-10 22:45:41 +0200

    .

--- backend/APIApplication/ApiApplication/Database/Data/PrisninjaDbContext.cs
+++ backend/APIApplication/ApiApplication/Database/Data/PrisninjaDbContext.cs
@@ -7,25 +7,25 @@ public class PrisninjaDbContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("" +
-                                    "Server=tcp:prj4server.database.windows.net,1433;" +
-                                    "Initial Catalog=PRJ4 Database;" +
-                                    "Persist Security Info=False;" +
-                                    "User ID=superadmin;" +
-                                    "Password=Superpassword1;" +
-                                    "MultipleActiveResultSets=False;" +
-                                    "Encrypt=True;" +
-                                    "TrustServerCertificate=False;" +
-                                    "Connection Timeout=30;"
-        );
-
         // optionsBuilder.UseSqlServer("" +
-        //                             "Data Source=" + ";" +
-        //                             "Database=TestDB;" +
-        //                             "TrustServerCertificate=true;" +
-        //                             "User ID=SA;" +
-        //                             "PASSWORD=<Tofirebananer147>"
+        //                             "Server=tcp:prj4server.database.windows.net,1433;" +
+        //                             "Initial Catalog=PRJ4 Database;" +
+        //                             "Persist Security Info=False;" +
+        //                             "User ID=superadmin;" +
+        //                             "Password=Superpassword1;" +
+        //                             "MultipleActiveResultSets=False;" +
+        //                             "Encrypt=True;" +
+        //                             "TrustServerCertificate=False;" +
+        //                             "Connection Timeout=30;"
         // );
+
+        optionsBuilder.UseSqlServer("" +
+                                    "Data Source=localhost;" +
+                                    "Database=TestDB;" +
+                                    "TrustServerCertificate=true;" +
+                                    "User ID=SA;" +
+                                    "PASSWORD=<Tofirebananer147>"
+        );
     }
 
     public DbSet<Store> Stores => Set<Store>();
