158dbdbd2c3596a18b885e9b82f1e45989e4225b
LukasMandrup <AU617683@post.au.dk>
2022-04-24 12:00:03 +0200

    Add ExternalApiLibrary.Test project

---
+++ backend/APIApplication/ExternalApiLibrary.Test/ExternalAPIComponent/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace ExternalApiLibrary.Test;
+
+public class Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Test1()
+	{
+		Assert.Pass();
+	}
+}
