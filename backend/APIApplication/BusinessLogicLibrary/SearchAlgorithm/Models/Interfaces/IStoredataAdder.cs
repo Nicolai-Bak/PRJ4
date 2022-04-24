ea25fd619e6fc3e2d424e8e7d6d90a38d6273bc6
Nikolaj Lærke <nikolaj.laerke@gmail.com>
2022-04-24 13:00:46 +0200

    refactored search algorithm to testable deisgn

---
+++ backend/APIApplication/BusinessLogicLibrary/SearchAlgorithm/Models/Interfaces/IStoredataAdder.cs
@@ -0,0 +1,9 @@
+﻿using ApiApplication.Database.Models;
+using ApiApplication.SearchAlgorithm.Models;
+
+namespace BusinessLogicLibrary.SearchAlgorithm.Models.Interfaces;
+
+public interface IStoredataAdder
+{
+    public void AddStoredata(List<Store> storeData, List<StoreSearch> stores, double userLocationX, double userLocationY);
+}
---
+++ backend/APIApplication/BusinessLogicLibrary/SearchAlgorithm/Models/StoredataAdder.cs
@@ -0,0 +1,31 @@
+﻿using ApiApplication.Database.Models;
+using ApiApplication.SearchAlgorithm.Models;
+using BusinessLogicLibrary.SearchAlgorithm.Models.Interfaces;
+
+namespace BusinessLogicLibrary.SearchAlgorithm.Models;
+
+public class StoredataAdder: IStoredataAdder
+{
+    public void AddStoredata(List<Store> storeData, List<StoreSearch> stores, double userLocationX, double userLocationY)
+    {
+        foreach (Store store in storeData)
+        {
+            foreach (var storeSearch in stores)
+            {
+                if (storeSearch.StoreID == store.ID)
+                {
+                    storeSearch.Address = store.Address;
+                    storeSearch.Brand = store.Brand;
+                    storeSearch.Distance = GetDistance(store.Location_X, store.Location_Y, userLocationX, userLocationY);
+                }
+            }
+        }
+    }
+    
+    private double GetDistance(double storeX, double storeY, double customerX, double customerY)
+    {
+        return Math.Sqrt(
+            Math.Pow(Math.Abs(storeX - customerX), 2) +
+            Math.Pow(Math.Abs(storeY - customerY), 2));
+    }
+}
