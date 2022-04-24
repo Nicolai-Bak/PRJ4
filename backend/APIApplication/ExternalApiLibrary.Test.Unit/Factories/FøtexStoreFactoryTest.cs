d1198f1fc36fc69f2a534b5efd84bbd064f5ec8f
Nicolai-Bak <nicolai.bak@gmail.com>
2022-04-24 19:58:51 +0200

    factory test made

--- backend/APIApplication/ExternalApiLibrary.Test.Unit/Factories/FøtexStoreFactoryTest.cs
+++ backend/APIApplication/ExternalApiLibrary.Test.Unit/Factories/FøtexStoreFactoryTest.cs
@@ -7,32 +7,32 @@ using NUnit.Framework;
 namespace ExternalApiLibrary.Test.Unit.Factories;
 
 [TestFixture]
-public class FøtexProductFactoryTest
+public class FøtexStoreFactoryTest
 {
     private IApiFactory _uut;
 
     [SetUp]
     public void Setup()
     {
-        _uut = new FøtexProductFactory();
+        _uut = new FøtexStoreFactory();
     }
 
     [Test]
     public void CreateCallerTest()
     {
         var returnValue = _uut.CreateCaller();
-        Assert.AreEqual( returnValue.GetType(), typeof(SallingProductCaller));
+        Assert.AreEqual( returnValue.GetType(), typeof(SallingStoreCaller));
     }
     [Test]
     public void CreateFilterTest()
     {
         var returnValue = _uut.CreateFilter();
-        Assert.AreEqual( returnValue.GetType(), typeof(SallingProductFilter));
+        Assert.AreEqual( returnValue.GetType(), typeof(SallingStoreFilter));
     }
     [Test]
     public void CreateConverterTest()
     {
         var returnValue = _uut.CreateConverter();
-        Assert.AreEqual( returnValue.GetType(), typeof(SallingProductConverter));
+        Assert.AreEqual( returnValue.GetType(), typeof(SallingStoreConverter));
     }
 }
