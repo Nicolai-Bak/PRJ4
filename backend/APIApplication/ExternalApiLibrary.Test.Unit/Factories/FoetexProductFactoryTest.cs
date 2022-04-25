4e7449fd94bd2fcd0bbdcb7d0a61fecde71f2478
LukasM <au617683@post.au.dk>
2022-04-25 10:58:16 +0200

    Fix file names on føtex tests

--- backend/APIApplication/ExternalApiLibrary.Test.Unit/Factories/FoetexProductFactoryTest.cs
+++ backend/APIApplication/ExternalApiLibrary.Test.Unit/Factories/FoetexProductFactoryTest.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 namespace ExternalApiLibrary.Test.Unit.Factories;
 
 [TestFixture]
-public class FøtexProductFactoryTest
+public class FoetexProductFactoryTest
 {
     private IApiFactory _uut;
 
