dd740794010cbf371f334a195e66ec17c1173ff8
Nicolai-Bak <nicolai.bak@gmail.com>
2022-04-28 17:00:02 +0200

    filter added to coop product caller

--- backend/APIApplication/ExternalApiLibrary/Callers/Interfaces/ICaller.cs
+++ backend/APIApplication/ExternalApiLibrary/Callers/Interfaces/ICaller.cs
@@ -1,6 +1,8 @@
+using ExternalApiLibrary.DTO;
+
 namespace ExternalApiLibrary.Callers.Interfaces;
 
 public interface ICaller
 {
-	public Task<List<object>> Call();
+	public Task<List<IFilteredDto>> Call();
 }
