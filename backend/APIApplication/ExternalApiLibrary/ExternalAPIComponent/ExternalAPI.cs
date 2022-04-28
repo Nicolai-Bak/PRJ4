84b20b664097b7840d144cb0fd9f366d0ebf20bb
Nicolai-Bak <nicolai.bak@gmail.com>
2022-04-28 17:01:36 +0200

    Filter removed from external api class and interface

--- backend/APIApplication/ExternalApiLibrary/ExternalAPIComponent/ExternalAPI.cs
+++ backend/APIApplication/ExternalApiLibrary/ExternalAPIComponent/ExternalAPI.cs
@@ -7,17 +7,15 @@ namespace ExternalApiLibrary.ExternalAPIComponent;
 public class ExternalApi: IExternalApi
 {
     public ICaller caller { private get; set; }
-    public IFilter filter { private get; set; }
     public IConverter converter { private get; set; }
 
     public ExternalApi(IApiFactory factory)
     {
         caller = factory.CreateCaller();
-        filter = factory.CreateFilter();
         converter = factory.CreateConverter();
     }
     public async Task<List<object>> Get()
     {
-        return converter.Convert(filter.Filter(await caller.Call()));
+        return converter.Convert(await caller.Call());
     }
 }
--- backend/APIApplication/ExternalApiLibrary/IExternalApi.cs
+++ backend/APIApplication/ExternalApiLibrary/IExternalApi.cs
@@ -7,7 +7,6 @@ namespace ExternalApiLibrary;
 public interface IExternalApi
 {
     public ICaller caller { set; }
-    public IFilter filter { set; }
     public IConverter converter { set; }
     public Task<List<object>> Get();
 }
