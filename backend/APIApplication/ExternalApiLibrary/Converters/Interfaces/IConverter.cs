8f3c1767fb4e882138c98e7123672955b1bf09c5
Nicolai-Bak <nicolai.bak@gmail.com>
2022-04-28 17:00:46 +0200

    IConverter argument updated

--- backend/APIApplication/ExternalApiLibrary/Converters/Interfaces/IConverter.cs
+++ backend/APIApplication/ExternalApiLibrary/Converters/Interfaces/IConverter.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DatabaseLibrary.Models;
 using ExternalApiLibrary.DTO;
 
 namespace ExternalApiLibrary.Converters.Interfaces
 {
     public interface IConverter
     {
-        public List<IDbModelsDto> Convert(List<IFilteredDto> list);
+        public List<object> Convert(List<IFilteredDto> list);
     }
 }
