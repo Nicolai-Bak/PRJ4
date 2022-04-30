894535f69421e3b29f6c431b61252d8177a3d528
LukasMandrup <AU617683@post.au.dk>
2022-04-30 12:23:28 +0200

    Fix test failures and rename test project

--- backend/APIApplication/ExternalApiLibrary/Callers/Coop/CoopStoreCaller.cs
+++ backend/APIApplication/ExternalApiLibrary/Callers/Coop/CoopStoreCaller.cs
@@ -7,7 +7,7 @@ namespace ExternalApiLibrary.Callers.Coop;
 
 public class CoopStoreCaller : ICaller
 {
-    private static readonly List<string> StoresToRetrieve =
+    public static readonly List<string> StoresToRetrieve =
         new() { "Kvickly", "SuperBrugsen", "DagliBrugsen", "Irma", "Fakta", "Coop365" };
     private static readonly string BaseUrl = "https://info.coop.dk/umbraco/surface/Chains/GetAllStores";
     private IRequest _request;
