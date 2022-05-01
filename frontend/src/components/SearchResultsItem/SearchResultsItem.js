9fec1394b00350becd70d448a09df9bb5a5be93b
Peter-Trang <201807770@post.au.dk>
2022-05-01 12:36:19 +0200

    updated dropdown, searchresultspage so options change depending on selected search option.

--- frontend/src/components/SearchResultsItem/SearchResultsItem.js
+++ frontend/src/components/SearchResultsItem/SearchResultsItem.js
@@ -3,8 +3,9 @@ import "./SearchResultsItem.css";
 import { IoChevronDownSharp } from "react-icons/io5";
 import Button from "../UI/Atoms/Button/Button";
 import {useState} from "react";
+import SearchResultsItemProduct from "../SearchResultsItemProduct/SearchResultsItemProduct";
 
-const SearchResultsItem = (props) => {
+const SearchResultsItem = (props, products=[]) => {
     const [activeState, setActiveState] = useState(true);
 
     const toggleDetails = () => {
@@ -15,14 +16,19 @@ const SearchResultsItem = (props) => {
         <div className="search-results-item">
             <Card className="search-results-list-item">
             <div className="list-item-wrapper">
-             <div className="list-item-price">{props.price}</div>
-             <div className="list-item-stores">{props.stores}</div>
-             <div className="list-item-distance">{props.distance}</div>
+             <div className="list-item-price">Pris: {props.price}kr</div>
+             <div className="list-item-store-name">{props.storeName}</div>
+             <div className="list-item-distance">Afstand: {props.distance}km</div>
              <Button className="chevron-button" onClick={toggleDetails}>
                 <IoChevronDownSharp className={`chevron-icon ${!activeState ? "rotate" : ""}`}/>
             </Button>
             </div>
-            <div className={`search-results-details ${!activeState ? "show" : ""}`}>{props.results}</div>
+            <div className={`search-results-details ${!activeState ? "show" : ""}`}>
+            {/* {products.map(product => (
+                    <SearchResultsItemProduct results={product.name}/>
+            ))} */}
+                <SearchResultsItemProduct results={props.results}/>
+</div>
             </Card>
         </div>
     );
