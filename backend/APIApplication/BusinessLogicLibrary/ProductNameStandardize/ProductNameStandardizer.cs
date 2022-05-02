2d9c71212f3324f192fc29998746ede0acef2547
Aksel <akselb@live.dk>
2022-05-02 09:18:31 +0200

    Refactored ProductNameStandardizer.cs

--- backend/APIApplication/BusinessLogicLibrary/ProductNameStandardize/ProductNameStandardizer.cs
+++ backend/APIApplication/BusinessLogicLibrary/ProductNameStandardize/ProductNameStandardizer.cs
@@ -2,13 +2,39 @@
 
 namespace BusinessLogicLibrary.ProductNameStandardize;
 
-public class ProductNameStandardizer
+public class ProductNameStandardizer : IProductNameStandardizer
 {
     public List<ProductStandardName> Standardize(List<Product> products)
     {
+        var productList = FilterProductNames(products);
+        
         var standardList = new List<ProductStandardName>();
+        productList.ForEach(p =>
+        {
+            var oldPsn = FindSimilarProductStandardName(standardList, p);
+            var psn = new ProductStandardName
+            {
+                Name = p.Name,
+                MeasureG = (p.Measurement.ToLower().Contains("g") ? true : false),
+                MeasureL = (p.Measurement.ToLower().Contains("l") ? true : false),
+                MeasureStk = (p.Measurement.ToLower().Contains("stk") ? true : false),
+                Organic = p.Organic
+            };
+            psn.MeasureG |= oldPsn.MeasureG;
+            psn.MeasureL |= oldPsn.MeasureL;
+            psn.MeasureStk |= oldPsn.MeasureStk;
+            psn.Organic |= oldPsn.Organic;
 
-        products.ForEach(p =>
+            standardList.Remove(oldPsn);
+            standardList.Add(psn);
+        });
+        return standardList;
+    }
+
+    private List<Product> FilterProductNames(List<Product> products)
+    {
+        var pList = products;
+        pList.ForEach(p =>
         {
             p.Name = p.Name
                 .ToLower()
@@ -20,39 +46,28 @@ public class ProductNameStandardizer
                 .Replace("  ", " ")
                 .Trim();
         });
+        return pList;
+    }
 
-        products.ForEach(p =>
+    private ProductStandardName FindSimilarProductStandardName(List<ProductStandardName> standardList, Product p)
+    {
+        if (standardList.Any(sn => sn.Name == p.Name))
         {
-            var oldPsn = new ProductStandardName();
-            var psn = new ProductStandardName()
-            {
-                Name = p.Name,
-                MeasureG = (p.Measurement.ToLower().Contains("g") ? true : false),
-                MeasureL = (p.Measurement.ToLower().Contains("l") ? true : false),
-                MeasureStk = (p.Measurement.ToLower().Contains("stk") ? true : false),
-                Organic = p.Organic
-            };
-            if (standardList.Any(sn => sn.Name == p.Name))
-            {
-                oldPsn = standardList.Find(sn => sn.Name == p.Name);
-            }
-            else if (standardList.Any(sn => sn.Name.Contains(p.Name + " ")))
-            {
-                oldPsn = standardList.Find(sn => sn.Name.Contains(p.Name + " "));
-            }
-            else if (standardList.Any(sn => p.Name.Contains(sn.Name + " ")))
-            {
-                oldPsn = standardList.Find(sn => p.Name.Contains(sn.Name + " "));
-                psn.Name = oldPsn.Name;
-            }
-            psn.MeasureG |= oldPsn.MeasureG;
-            psn.MeasureL |= oldPsn.MeasureL;
-            psn.MeasureStk |= oldPsn.MeasureStk;
-            psn.Organic |= oldPsn.Organic;
-
-            standardList.Remove(oldPsn);
-            standardList.Add(psn);
-        });
-        return standardList;
+            return standardList.Find(sn => sn.Name == p.Name);
+        }
+        else if (standardList.Any(sn => (" " + sn.Name + " ").Contains(" " + p.Name + " ")))
+        {
+            return standardList.Find(sn => (" " + sn.Name + " ").Contains(" " + p.Name + " "));
+        }
+        else if (standardList.Any(sn => (" " + p.Name + " ").Contains(" " + sn.Name + " ")))
+        {
+            var oldPsn = standardList.Find(sn => (" " + p.Name + " ").Contains(" " + sn.Name + " "));
+            p.Name = oldPsn.Name;
+            return oldPsn;
+        }
+        else
+        {
+            return new ProductStandardName();
+        }
     }
 }
