4fbcd5cbcc6a8b634d0a278025122f57f9ff28a6
LukasMandrup <AU617683@post.au.dk>
2022-05-08 18:30:35 +0200

    Add Coop Store Converter validation and logging

--- backend/APIApplication/ExternalApiLibrary/Converters/Coop/CoopStoreConverter.cs
+++ backend/APIApplication/ExternalApiLibrary/Converters/Coop/CoopStoreConverter.cs
@@ -2,6 +2,7 @@ using DatabaseLibrary.Models;
 using ExternalApiLibrary.Converters.Interfaces;
 using ExternalApiLibrary.DTO;
 using ExternalApiLibrary.Models;
+using Serilog;
 
 namespace ExternalApiLibrary.Converters.Coop;
 
@@ -9,7 +10,7 @@ public class CoopStoreConverter : IConverter
 {
     public List<IDbModelsDto> Convert(List<IFilteredDto> list)
     {
-        var filteredList = list.Cast<FilteredCoopStore>().ToList();
+        var filteredList = ValidateStores(list.Cast<FilteredCoopStore>().ToList());
         
         var stores = filteredList.Select(store => new Store
         {
@@ -22,4 +23,63 @@ public class CoopStoreConverter : IConverter
 
         return new List<IDbModelsDto>(stores);
     }
+
+    private List<FilteredCoopStore> ValidateStores(List<FilteredCoopStore>? stores)
+    {
+	    var validStores = new List<FilteredCoopStore>();
+
+	    stores?.ForEach(store =>
+	    {
+		    bool isValid = true;
+		    
+		    if (store.Kardex <= 0)
+		    {
+			    LogInvalidStore(store, $"Kardex was less than or equal to 0");
+			    isValid = false;
+		    }
+		    if (string.IsNullOrEmpty(store.RetailGroupName) || string.IsNullOrWhiteSpace(store.RetailGroupName))
+		    {
+			    LogInvalidStore(store, $"Retail Group Name was empty");
+			    isValid = false;
+		    }
+		    if (store.Location[0] <= 0)
+		    {
+			    LogInvalidStore(store, $"X-coordinate was less than or equal to 0");
+			    isValid = false;
+		    }
+		    if (store.Location[1] <= 0)
+		    {
+			    LogInvalidStore(store, $"Y-coordinate was less than or equal to 0");
+			    isValid = false;
+		    }
+		    if (string.IsNullOrEmpty(store.Address) || string.IsNullOrWhiteSpace(store.Address))
+		    {
+			    LogInvalidStore(store, $"Address was empty");
+			    isValid = false;
+		    }
+		    if (store.Zipcode <= 0)
+		    {
+			    LogInvalidStore(store, $"Zipcode was less than or equal to 0");
+			    isValid = false;
+		    }
+		    if (string.IsNullOrEmpty(store.City) || string.IsNullOrWhiteSpace(store.City))
+		    {
+			    LogInvalidStore(store, $"City was empty");
+			    isValid = false;
+		    }
+
+
+		    if (isValid)
+			    validStores.Add(store);
+	    });
+
+	    return validStores;
+    }
+    
+    private void LogInvalidStore(FilteredCoopStore store, string msg)
+    {
+	    Log.Fatal("[CoopStoreConverter] Invalid store conversion error: {@msg}\nStore: {@store}",
+		    msg, store);
+    }
+ 
 }
