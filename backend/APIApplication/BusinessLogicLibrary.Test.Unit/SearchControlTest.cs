66039c97f9304f4a11426bfefeecbe79069792e2
Aksel <akselb@live.dk>
2022-05-10 13:36:58 +0200

    Added more tests to SearchControlTest.cs

--- backend/APIApplication/BusinessLogicLibrary.Test.Unit/SearchControlTest.cs
+++ backend/APIApplication/BusinessLogicLibrary.Test.Unit/SearchControlTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using ApiApplication.Controllers;
 using ApiApplication.SearchAlgorithm;
 using ApiApplication.SearchAlgorithm.Models;
@@ -8,6 +9,7 @@ using DatabaseLibrary;
 using DatabaseLibrary.Data;
 using DatabaseLibrary.Models;
 using Microsoft.EntityFrameworkCore;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace BusinessLogicLibrary.Test.Unit;
@@ -17,10 +19,12 @@ public class SearchControlTest
     private ISearchControl _uut;
     private IDbInsert _dbInsert;
     private IDbSearch _dbSearch;
+    private IRangeCalculator _rangeCalculator;
 
     [SetUp]
     public void Setup()
     {
+        _rangeCalculator = new RangeCalculator();
         PrisninjaDbContext context = GetDbContext();
         IRangeCalculator rangeCalculator = new RangeCalculator();
         _dbInsert = new PrisninjaDb(context, rangeCalculator);
@@ -31,21 +35,76 @@ public class SearchControlTest
                 new CheapestStoreSelecter(),
                 new ClosestStoreSelecter(),
                 new BestStoreSelecter()
-            }, rangeCalculator);
-    }
+            }, _rangeCalculator);
+    } 
 
     [Test]
-    public void FindStores()
+    public void FindStores_ProductsInDatabase_ResultsNotEmpty()
     {
         var result = _uut.FindStores();
         Assert.That(result[0], Is.Empty);
-        
+        Assert.That(result[1], Is.Empty);
+        Assert.That(result[2], Is.Empty);
+
         _dbInsert.InsertProducts(CreateTestProducts());
         _dbInsert.InsertStores(CreateTestStores());
         _dbInsert.InsertProductStores(CreateTestProductStores());
 
         result = _uut.FindStores();
         Assert.That(result[0], Is.Not.Empty);
+        Assert.That(result[1], Is.Not.Empty);
+        Assert.That(result[2], Is.Not.Empty);
+    }
+    
+    [Test]
+    public void FindStores_ProductsInDatabase_CheapestOptionFound()
+    {
+        var products = CreateTestProducts();
+        var stores = CreateTestStores();
+        var productStores = CreateTestProductStores();
+        _dbInsert.InsertProducts(products);
+        _dbInsert.InsertStores(stores);
+        _dbInsert.InsertProductStores(productStores);
+
+        var result = _uut.FindStores();
+        Assert.That(result[0][0].StoreID, Is.EqualTo(stores[1].ID));
+        Assert.That(result[0][1].StoreID, Is.EqualTo(stores[0].ID));
+    }
+    
+    [Test]
+    public void FindStores_ProductsInDatabase_NearestOptionFound()
+    {
+        var products = CreateTestProducts();
+        var stores = CreateTestStores();
+        var productStores = CreateTestProductStores();
+        _dbInsert.InsertProducts(products);
+        _dbInsert.InsertStores(stores);
+        _dbInsert.InsertProductStores(productStores);
+
+        var shoppingList = CreateTestShoppingList();
+        IRangeCalculator rangeCalculator = Substitute.For<IRangeCalculator>();
+        rangeCalculator.Distance(
+            stores[0].Location_X,
+            shoppingList.X,
+            stores[0].Location_Y,
+            shoppingList.Y).Returns(1);
+        rangeCalculator.Distance(
+            stores[1].Location_X,
+            shoppingList.X,
+            stores[1].Location_Y,
+            shoppingList.Y).Returns(2);
+
+        _uut = new SearchControl(shoppingList, _dbSearch,
+            new List<IStoreSelecter>()
+            {
+                new CheapestStoreSelecter(),
+                new ClosestStoreSelecter(),
+                new BestStoreSelecter()
+            }, rangeCalculator);
+
+        var result = _uut.FindStores();
+        Assert.That(result[1][0].StoreID, Is.EqualTo(stores[0].ID));
+        Assert.That(result[1][1].StoreID, Is.EqualTo(stores[1].ID));
     }
 
     private PrisninjaDbContext GetDbContext()
@@ -73,6 +132,16 @@ public class SearchControlTest
                 Measurement = "g",
                 Organic = false,
                 ImageUrl = ""
+            },
+            new Product()
+            {
+                EAN = 124,
+                Name = "a",
+                Brand = "",
+                Units = 123,
+                Measurement = "g",
+                Organic = false,
+                ImageUrl = ""
             }
         };
     }
@@ -88,21 +157,39 @@ public class SearchControlTest
                 ID = 123,
                 Location_X = 123,
                 Location_Y = 123
+            },
+            new Store()
+            {
+                Address = "",
+                Brand = "",
+                ID = 124,
+                Location_X = 456,
+                Location_Y = 456
             }
         };
     }
 
     private List<ProductStore> CreateTestProductStores()
     {
+        var products = CreateTestProducts();
+        var stores = CreateTestStores();
         return new List<ProductStore>()
         {
             new ProductStore()
             {
                 Price = 123,
-                Store = CreateTestStores()[0],
-                StoreKey = CreateTestStores()[0].ID,
-                Product = CreateTestProducts()[0],
-                ProductKey = CreateTestProducts()[0].EAN
+                Store = stores[0],
+                StoreKey = stores[0].ID,
+                Product = products[0],
+                ProductKey = products[0].EAN
+            },
+            new ProductStore()
+            {
+                Price = 10,
+                Store = stores[1],
+                StoreKey = stores[1].ID,
+                Product = products[1],
+                ProductKey = products[1].EAN
             }
         };
     }
@@ -123,7 +210,7 @@ public class SearchControlTest
             },
             X = 123,
             Y = 123,
-            Range = 123
+            Range = 999999
         };
     }
 }
