db28d4b86a860bdc243bb80d1c0fbf9e2e7fd8df
LukasMandrup <AU617683@post.au.dk>
2022-05-15 18:42:38 +0200

    Add product validator test

---
+++ backend/APIApplication/ExternalApiLibrary.Test/Converters/ProductValidatorTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using DatabaseLibrary.Models;
+using ExternalApiLibrary.Converters;
+using NUnit.Framework;
+
+namespace ExternalApiLibrary.Test.Unit.Converters;
+
+public class ProductValidatorTest
+{
+	[SetUp]
+	public void Setup()
+	{
+		// Static class, no setup required
+	}
+
+	[Test]
+	public void ValidateProducts_ValidProduct_ReturnWholeList()
+	{
+		// Arrange
+		var product = new Product
+		{
+			EAN = 1,
+			Name = "Test",
+			Brand = "Test",
+			Units = 1.0,
+			Measurement = "Test",
+			Organic = true,
+			ImageUrl = "Test",
+			ProductStores = new List<ProductStore> { new() }
+		};
+
+		var products = new List<Product>
+		{
+			product
+		};
+
+		// Act
+		var result = ProductValidator.ValidateProducts(products, ProductGroup.Coop);
+
+		// Assert
+		Assert.AreEqual(products, result);
+	}
+	
+	[Test]
+	public void ValidateProducts_InvalidProduct_ReturnEmptyList()
+	{
+		// Arrange
+		var product = new Product
+		{
+			EAN = -1,
+			Units = -1.0,
+			ProductStores = new List<ProductStore>()
+		};
+
+		var products = new List<Product>
+		{
+			product
+		};
+
+		// Act
+		var result = ProductValidator.ValidateProducts(products, ProductGroup.Coop);
+
+		// Assert
+		Assert.That(result.Count, Is.EqualTo(0));
+	}
+}
